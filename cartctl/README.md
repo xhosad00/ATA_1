6d4ff79c1325c2adb33cb49e5d96110c9bb560fa
xhosad00 <80786156+xhosad00@users.noreply.github.com>
2025-04-11 00:35:00 +0200

    Late night push (before lecture)

--- cartctl/README.md
+++ cartctl/README.md
@@ -11,51 +11,11 @@ Rozporu ve Specifikace chování:
 Dále je požadována jako korektní varinata chování, že vozík v režimu UNLOAD_ONLY bude skutečně poze vykládat a jiný prioritní materiál nenaloží (tak jak je to v aktuální implementaci).
 Pokud by byla tato doměnka špatná, tak by vzrostla sekce UNLOAD_ONLY Mode o nakládání prioritního materiálu a vykládání více než jednoho materiálu.
 
-## Testovací cesty
+Nejasnosti:
+>F-10 The cart shall reject the cargo if all cart slots are occupied
+or loading exceeds the maximum capacity
 
-| Id | cesta/uzly   | pokryté požadavky |
-| :------------------ | :----------- | :---------------- |
-| 1| Cargo.dot: Created->Loaded | F-02|
-| 2| Cargo.dot: Created->Loaded->SetPriority |F-03|
-| 3|Cargo.dot: Created->Loaded-(Picked up)>SetPriority |F-04  |
-| 4|Cargo.dot: Created->Loaded-(After 60s, force pickup)>SetPriority |F-04  |
-| 5|CartStatus: Loading->UNLOAD_ONLY (UO) |F-05 |
-| 6|CartCtl: UNLOAD_ONLY->UNLOAD_ONLY |F-06 |
-| 7|CartCtl: UNLOAD_ONLY->NORMAL |F-07 |
-| 8|CartStatus: Moving->Moving |F-08|
-| 9|CartStatus: Loading->Loading->Loading |F-09|
-|10|CartStatus: Unloading->Unloading->Unloading |F-09|
-|11|CartStatus: Loading->Loading->Moving->Unloading->Unloading |F-09|
-|12|Cargo.dot: Created->Terminated |F-10|
-|13|CartStatus: IDLE->NORMAL |P-01|
-
-Je několik požadavků, které v cestách nejsou zahrnuty z následujících důvodů:
-F-01 - Neexistuje žádný modul, který by automaticky generoval nové požadavky
-F-08 - 
-
-
-## Vstupní parametry testů
-
-| Id | stručný popis |
-| :---------------------- | :------------ |
-| cartWeight | Maximální váha vozíku|
-| slots| Počet slotů vozíku |
-| cargoReq| Seznam n-tic reprezentující požadavek vytvořen ve formátu: [(src, dst, weight, name), ...]|
-| reqTime|Seznam časů, kdy byli vytvořené požadavky |
-
-
-## Tabulka testů
-
-
-| Id |cartWeight|slots|cargoReq|reqTime| očekávaný výsledek | pokryté testovací cesty |  název testovací metody |
-| :--- | :------ | :------- | :---- |:---- |:---- |:---- |:---- |
-| 1|150 |4 |[('A','B',30,'broccoli'), ('B','C',100,'carrot'), ('C','A',10,'daikon'), ('C','D',20,'onion')]| [1, 2, 70, 90] | Kontrolér příjmá požadavky v daných časech, postupně je úspěšně plní (nakládá a vykládá ve správných stanicích) a žádný materiál se nestane prioritním | | test_happy_no_prio|
-| 2|150 |4 |[('B','D',150,'broccoli'), ('D','B',30,'carrot'), ('A','B', 40,'daikon')]| [1, 2, 70] |Kontrolér příjmá požadavky v daných časech, postupně je úspěšně plní. Materiál `carrot` se stane prioritní. Po cestě na stanici vykládání `carrot` se nenaloží `daikon` kvůli UNLOAD_ONLY, i když má vozík volný slot a kapacitu a `daikon` je na cestě | | test_happy_prio|
-|3|[50, 150, 500]|[0, 1, 2, 3, 4, 5]|-|-|Postupně zkouší kombinace vozíku. Když je zvolená špatná kombinace, očekává CartError|-|test_cart_props_slots|
-|4|2|[0, 1, 50, 99.9, 150, 200, 500, 501]|-|-|Postupně zkouší kombinace vozíku. Když je zvolená špatná kombinace, očekává CartError|-|test_cart_props_weight|
-|5|150|4|[('A','D',50,'broccoli'), ('A','D',1000,'bigBroccoli'), ('A','D',-1,'bigBroccoli')]|0|Vytvoří několik požadavků. U přidávání několika nevalidních požadavků očekává CartError||test_cart_props_bad_req|
-
-
+Není jasné, jestli se požadavek vztahuje na požadavek už při jeho vytvoření, nebo až při nakládání 
 
 ## Diagramy
 
@@ -85,3 +45,57 @@ Jednotlivé stavové diagramy
 > 
 > Stavy Moving a Unloading v podgrafu UNLOAD ONLY Mode figurují stejně jako v normálním provozu, až na to, že při vyložení prioritního materiálu se hranou `All prio cargo unloaded` přesune do stavu NORMAL.
 > ![alt text](./diagrams/CartStatus.png "CartStatus")
+
+
+## Testovací cesty
+Zvolené pokrytí: **Edge Coverage**
+Cesta vždy patří k danému diagramu (specifikováno na začátku cesty)
+
+| Id | cesta/uzly   | pokryté požadavky |
+| :------------------ | :----------- | :---------------- |
+| 1| Cargo: Created->Loaded | F-02|
+| 2| Cargo: Created->Loaded->SetPriority |F-03|
+| 3|Cargo: Created->SetPriority-(Picked up)>Loaded |F-04  |
+| 4|Cargo: Created->SetPriority-(After 60s, force pickup)>Loaded |F-04  |
+| 5|CartStatus: Loading->UNLOAD_ONLY (UO) |F-05 |
+| 6|CartCtl: UNLOAD_ONLY->UNLOAD_ONLY |F-06 |
+| 7|CartCtl: UNLOAD_ONLY->NORMAL |F-07 |
+| 8|CartStatus: Moving->Moving |F-08|
+| 9|CartStatus: Loading->Loading->Loading |F-09|
+|10|CartStatus: Unloading->Unloading->Unloading |F-09|
+|11|CartStatus: Loading->Loading->Moving->Unloading->Unloading |F-09|
+|12|Cargo: Created->Terminated |F-10 C-05|
+|13|CartStatus: IDLE->NORMAL |P-01|
+|20|CartStatus: NORMAL->IDLE|-|
+|21|CartStatus: NORMAL->Loading->Loading->Moving->Moving->Unloading->Unloading->Loading->Loading->Loading->Moving->Moving->Unloading->Unloading->Moving->Moving->Unloading->Unloading->NORMAL->IDLE|-|
+|22|CartStatus: NORMAL->Moving->Moving->Loading|-|
+|23|CartStatus: Loading->UNLOAD_ONLY (UO)->(UO) Moving->(UO) Moving->(UO) Unloading->(UO) Unloading->NORMAL|-|
+
+Požadavky 20-23 slouží pro splnění kritéria **Edge Coverage**
+
+Je několik požadavků, které v cestách nejsou zahrnuty z následujících důvodů:
+* F-01 - Neexistuje žádný modul, který by automaticky generoval nové požadavky
+* F-08 - Diagram nezahrnuje jednotlivé stanice a požadavky. Cesta splňující/vyvracující tuto vlastnost by byla nejasná.
+
+
+## Vstupní parametry testů
+
+| Id | stručný popis |
+| :---------------------- | :------------ |
+| cartWeight | Maximální váha vozíku|
+| slots| Počet slotů vozíku |
+| cargoReq| Seznam n-tic reprezentující požadavek vytvořen ve formátu: [(src, dst, weight, name), ...]|
+| reqTime|Seznam časů, kdy byli vytvořené požadavky |
+
+## Tabulka testů
+
+
+| Id |cartWeight|slots|cargoReq|reqTime| očekávaný výsledek | pokryté test. cesty / požadavky |  název testovací metody |
+| :--- | :------ | :------- | :---- |:---- |:---- |:---- |:---- |
+| 1|150 |4 |[('A','B',30,'broccoli'), ('B','C',100,'carrot'), ('C','A',10,'daikon'), ('C','D',20,'onion')]| [1, 2, 70, 90] | Kontrolér příjmá požadavky v daných časech, postupně je úspěšně plní (nakládá a vykládá ve správných stanicích) a žádný materiál se nestane prioritním |1 8 9 13 20 21| test_happy_no_prio|
+| 2|150 |4 |[('B','D',150,'broccoli'), ('D','B',30,'carrot'), ('A','B', 40,'daikon')]| [1, 2, 70] |Kontrolér příjmá požadavky v daných časech, postupně je úspěšně plní. Materiál `carrot` se stane prioritní. Po cestě na stanici vykládání `carrot` se nenaloží `daikon` kvůli UNLOAD_ONLY, i když má vozík volný slot a kapacitu a `daikon` je na cestě |1 2 3 5 6 7 8 13 20 22 23| test_happy_prio|
+|3|[50, 150, 500]|[0, 1, 2, 3, 4, 5]|-|-|Postupně zkouší kombinace vozíku. Když je zvolená špatná kombinace, očekává CartError|C-01 C-03 C-04|test_cart_props_slots|
+|4|2|[0, 1, 50, 99.9, 150, 200, 500, 501]|-|-|Postupně zkouší kombinace vozíku. Když je zvolená špatná kombinace, očekává CartError|C-02|test_cart_props_weight|
+|5|500|2|[('A','D',50,'broccoli'), ('A','D',1000,'bigBroccoli'), ('A','D',-1,'bigBroccoli')]|0|Vytvoří několik požadavků. U přidávání několika nevalidních požadavků očekává CartError|12|test_cart_props_bad_req|
+|6|150 |4 |[('A','B',20,'broccoli'), ('B','A',30,'carrot'), ('B','D', 40,'daikon')]| [1, 2, 3] |Testuje zda je cesta skutečně optimální (na lehkém příkladu)|8| test_optimize_total_path|
+
--- cartctl/tests/test_cartctl.py
+++ cartctl/tests/test_cartctl.py
@@ -5,102 +5,284 @@ Example of usage/test of Cart controller implementation.
 
 import sys
 import unittest
+# from cartctl.cartctl.cartctl import CartCtl
 from cartctl.cartctl import CartCtl
-from cartctl.cart import Cart, CargoReq
+from cartctl.cartctl import Status as CartCtlStatus
+from cartctl.cart import Cart, CargoReq, CartError
+from cartctl.cart import Status as CartStatus
 from cartctl.jarvisenv import Jarvis
 
 def log(msg):
     "simple logging"
     print('  %s' % msg)
 
+# Basic functions with no asserts
+def add_load(c: CartCtl, cargo_req: CargoReq):
+    log('%d: Requesting %s at %s' % \
+        (Jarvis.time(), cargo_req, cargo_req.src))
+    c.request(cargo_req)
+
+def on_move(c: Cart):
+    log('%d: Cart is moving %s->%s' % (Jarvis.time(), c.pos, c.data))
+
+def on_load(c: Cart, cargo_req: CargoReq):
+    log('%d: Cart at %s: loading: %s' % (Jarvis.time(), c.pos, cargo_req))
+    log(c)
+    cargo_req.context = "loaded"
+
+def on_unload(c: Cart, cargo_req: CargoReq):
+    log('%d: Cart at %s: unloading: %s' % (Jarvis.time(), c.pos, cargo_req))
+    log(c)
+    cargo_req.context = "unloaded"
+
+def createBasicCargo(src, dst, weight, name):
+    req = CargoReq(src, dst, weight, name)
+    req.onload = on_load
+    req.onunload = on_unload
+    return req
+    
 class TestCartRequests(unittest.TestCase):
 
-    def test_happy(self):
-        "Happy-path test"
+    def test_happy_no_prio(self):
+        "Happy-path with no priority cargo"
+        print("")
 
-        def add_load(c: CartCtl, cargo_req: CargoReq):
-            "callback for schedulled load"
-            log('%d: Requesting %s at %s' % \
-                (Jarvis.time(), cargo_req, cargo_req.src))
-            c.request(cargo_req)
-
+        
         def on_move(c: Cart):
-            "example callback (for assert)"
-            # put some asserts here
             log('%d: Cart is moving %s->%s' % (Jarvis.time(), c.pos, c.data))
+            self.assertEqual(CartStatus.MOVING, cart.status)
+            self.assertFalse(cart.empty())  #during entire run, the cart should have some cargo
 
         def on_load(c: Cart, cargo_req: CargoReq):
-            "example callback for logging"
             log('%d: Cart at %s: loading: %s' % (Jarvis.time(), c.pos, cargo_req))
             log(c)
+            self.assertFalse(cart.empty())
+            self.assertEqual(None, cargo_req.context)
+            cargo_req.context = "loaded"
+            if cargo_req.content == 'broccoli':
+                self.assertEqual('A', c.pos)
+                self.assertEqual(cargo_req.weight, c.load_sum())
+            if cargo_req.content == 'carrot':
+                self.assertEqual('B', c.pos)
+            if cargo_req.content == 'daikon':
+                self.assertEqual('C', c.pos)
+            if cargo_req.content == 'onion':
+                self.assertEqual('C', c.pos)
+
+        def on_unload(c: Cart, cargo_req: CargoReq):
+            log('%d: Cart at %s: unloading: %s' % (Jarvis.time(), c.pos, cargo_req))
+            log(c)
+            self.assertEqual("loaded", cargo_req.context)
+            cargo_req.context = "unloaded"
+            if cargo_req.content == 'broccoli':
+                self.assertEqual('B', c.pos)
+            if cargo_req.content == 'carrot':
+                self.assertEqual('C', c.pos)
+            if cargo_req.content == 'daikon':
+                self.assertEqual('A', c.pos)
+            if cargo_req.content == 'onion':
+                self.assertEqual('D', c.pos)
+
+        def createBasicCargo(src, dst, weight, name):
+            req = CargoReq(src, dst, weight, name)
+            req.onload = on_load
+            req.onunload = on_unload
+            return req
+
+        cart = Cart(4, 150, 0)
+        cart.onmove = on_move
+        ctl = CartCtl(cart, Jarvis)
+        broccoli = createBasicCargo('A', 'B', 30, 'broccoli')
+        carrot = createBasicCargo('B', 'C', 100, 'carrot')
+        daikon = createBasicCargo('C', 'A', 10, 'daikon')
+        onion = createBasicCargo('C', 'D', 20, 'onion')
+
+        Jarvis.reset_scheduler()
+        Jarvis.plan(0, add_load, (ctl,broccoli))
+        Jarvis.plan(30, add_load, (ctl,carrot))
+        Jarvis.plan(35, add_load, (ctl,daikon))
+        Jarvis.plan(38, add_load, (ctl,onion))
+        Jarvis.run()
+
+        log(cart)
+        self.assertTrue(cart.empty())
+        self.assertEqual('unloaded', broccoli.context)
+        self.assertEqual('unloaded', carrot.context)
+        self.assertEqual('unloaded', daikon.context)
+        self.assertEqual('unloaded', onion.context)
+        self.assertEqual(cart.pos, 'A')
+
+
+    def test_happy_prio(self):
+        "Happy-path with priority cargo"
+        print("\n")
+
+        def on_move(c: Cart):
+            log('%d: Cart is moving %s->%s' % (Jarvis.time(), c.pos, c.data))
+            self.assertEqual(CartStatus.MOVING, cart.status)
+
+        def on_load(c: Cart, cargo_req: CargoReq):
+            log('%d: Cart at %s: loading: %s' % (Jarvis.time(), c.pos, cargo_req))
+            log(c)
+            self.assertFalse(cart.empty())
+            self.assertEqual(None, cargo_req.context)
             cargo_req.context = "loaded"
 
         def on_unload(c: Cart, cargo_req: CargoReq):
-            "example callback (for assert)"
-            # put some asserts here
             log('%d: Cart at %s: unloading: %s' % (Jarvis.time(), c.pos, cargo_req))
             log(c)
-            self.assertEqual('loaded', cargo_req.context)
-            cargo_req.context = 'unloaded'
-            if cargo_req.content == 'helmet':
+            cargo_req.context = "unloaded"
+            if cargo_req.content == 'carrot':
                 self.assertEqual('B', c.pos)
-            if cargo_req.content == 'heart':
-                self.assertEqual('A', c.pos)
-            #if cargo_req.content.startswith('bracelet'):
-            #    self.assertEqual('C', c.pos)
-            if cargo_req.content == 'braceletR':
-                self.assertEqual('A', c.pos)
-            if cargo_req.content == 'braceletL':
-                self.assertEqual('C', c.pos)
+                self.assertEqual(0, c.load_sum()) # check that daikon wasnt loaded (Unload_Only)
 
-        # Setup Cart
-        # 4 slots, 150 kg max payload capacity, 2=max debug
-        cart_dev = Cart(4, 150, 0)
-        cart_dev.onmove = on_move
-
-        # Setup Cart Controller
-        c = CartCtl(cart_dev, Jarvis)
-
-        # Setup Cargo to move
-        helmet = CargoReq('A', 'B', 20, 'helmet')
-        helmet.onload = on_load
-        helmet.onunload = on_unload
-
-        heart = CargoReq('C', 'A', 40, 'heart')
-        heart.onload = on_load
-        heart.onunload = on_unload
-
-        braceletR = CargoReq('D', 'A', 40, 'braceletR')
-        braceletR.onload = on_load
-        braceletR.onunload = on_unload
-
-        braceletL = CargoReq('D', 'C', 40, 'braceletL')
-        braceletL.onload = on_load
-        braceletL.onunload = on_unload
-
-        # Setup Plan
-        Jarvis.reset_scheduler()
-        #         when  event     called_with_params
-        Jarvis.plan(10, add_load, (c,helmet))
-        Jarvis.plan(45, add_load, (c,heart))
-        Jarvis.plan(40, add_load, (c,braceletR))
-        Jarvis.plan(25, add_load, (c,braceletL))
+        def createBasicCargo(src, dst, weight, name):
+            req = CargoReq(src, dst, weight, name)
+            req.onload = on_load
+            req.onunload = on_unload
+            return req
         
-        # Exercise + Verify indirect output
-        #   SUT is the Cart.
-        #   Exercise means calling Cart.request in different time periods.
-        #   Requests are called by add_load (via plan and its scheduler).
-        #   Here, we run the plan.
+        def checkUNLOAD_ONLY(ctl):
+            self.assertEqual(CartCtlStatus.UNLOAD_ONLY, ctl.status)
+
+        cart = Cart(4, 150, 0)
+        cart.onmove = on_move
+        ctl = CartCtl(cart, Jarvis)
+        
+        broccoli = createBasicCargo('B', 'D', 150, 'broccoli')
+        carrot = createBasicCargo('D', 'B', 30, 'carrot')
+        daikon = createBasicCargo('A', 'B', 40, 'daikon')
+
+        Jarvis.reset_scheduler()
+        Jarvis.plan(1, add_load, (ctl,broccoli))
+        Jarvis.plan(2, add_load, (ctl,carrot))
+        Jarvis.plan(70, add_load, (ctl,daikon))
+        Jarvis.plan(90, checkUNLOAD_ONLY, (ctl,))
         Jarvis.run()
 
-        # Verify direct output
-        log(cart_dev)
-        self.assertTrue(cart_dev.empty())
-        self.assertEqual('unloaded', helmet.context)
-        self.assertEqual('unloaded', heart.context)
-        self.assertEqual('unloaded', braceletR.context)
-        self.assertEqual('unloaded', braceletL.context)
-        #self.assertEqual(cart_dev.pos, 'C')
+        # # Verify direct output
+        log(cart)
+        self.assertTrue(cart.empty())
+        self.assertEqual('unloaded', broccoli.context)
+        self.assertEqual('unloaded', carrot.context)
+        self.assertEqual('unloaded', daikon.context)
+        # self.assertEqual(cart.pos, 'D')
+
+    def test_optimize_total_path(self):
+        """Require good path optimization"""
+        # optimezed path:           A->B->C->D->A 70
+        # worse path (due to UCS):  A->B->A->B->C->D
+
+        cart = Cart(4, 150, 0)
+        cart.onmove = on_move
+        ctl = CartCtl(cart, Jarvis)
+
+        broccoli = createBasicCargo('A', 'B', 20, 'broccoli')
+        carrot = createBasicCargo('B', 'A', 30, 'carrot')
+        daikon = createBasicCargo('B', 'D', 40, 'daikon')
+        Jarvis.reset_scheduler()
+        Jarvis.plan(1, add_load, (ctl,broccoli))
+        Jarvis.plan(2, add_load, (ctl,carrot))
+        Jarvis.plan(3, add_load, (ctl,daikon))
+        Jarvis.run()
+        self.assertEqual(cart.pos, 'A')
+
+        cart = Cart(4, 150, 0)
+        ctl = CartCtl(cart, Jarvis)
+
+    def test_cart_props_slots(self):
+        """test cart slot restrtictions"""
+        # "C-01 The cart shall have a capacity of 1 to 4 slots"
+        Cart(1, 150, 0)
+        Cart(2, 150, 0)
+        Cart(3, 150, 0)
+        Cart(4, 150, 0)
+        self.assertRaises(CartError, Cart, 0, 50, 0)
+        self.assertRaises(CartError, Cart, 5, 50, 0)
+
+        # C-03 Carts with the lowest capacity shall have at least 2 slots
+        self.assertRaises(CartError, Cart, 1, 50, 0)
+
+        # C-04 Carts with the highest capacity shall not have more than 2 slots.
+        Cart(1, 500, 0)
+        Cart(2, 500, 0)
+        self.assertRaises(CartError, Cart, 3, 500, 0)
+
+    def test_cart_props_weight(self):
+        """test cart weight restrtictions"""
+        # C-02 The cart’s maximum capacity shall correspond to its type (50kg, 150 kg, or 500 kg).
+        Cart(2, 50, 0)
+        Cart(2, 150, 0)
+        Cart(2, 500, 0)
+        self.assertRaises(CartError, Cart, 2, 0, 0)
+        self.assertRaises(CartError, Cart, 2, 1, 0)
+        self.assertRaises(CartError, Cart, 2, 200, 0)
+        self.assertRaises(CartError, Cart, 2, 501, 0)
+        self.assertRaises(CartError, Cart, 2, 99.9, 0)
+
+    def test_cart_props_bad_req(self):
+        """test cart bad requests restrtictions"""
+        # C-05 The control system shall not submit invalid requests (e.g.,cargo transfer exceeding the cart’s capacity).
+        def add_load(c: CartCtl, cargo_req: CargoReq):
+            c.request(cargo_req)
+
+        def add_load_err(c: CartCtl, cargo_req: CargoReq):
+            self.assertRaises(CartError, c.request, cargo_req)
+
+        cart = Cart(2, 500, 0)
+        ctl = CartCtl(cart, Jarvis)
+
+        
+        broccoli = CargoReq('A', 'D', 50, 'broccoli')
+        Jarvis.reset_scheduler()
+        Jarvis.plan(0, add_load, (ctl,broccoli))
+        Jarvis.run()
+        
+        # Ok
+        cart = Cart(2, 500, 0)
+        ctl = CartCtl(cart, Jarvis)        
+        bigBroccoli = CargoReq('A', 'D', 1000, 'bigBroccoli')
+        Jarvis.reset_scheduler()
+        Jarvis.plan(0, add_load_err, (ctl,bigBroccoli))
+        Jarvis.run()
+        
+        # Should throw errors
+        cart = Cart(2, 500, 0)
+        ctl = CartCtl(cart, Jarvis)        
+        negativeBroccoli = CargoReq('A', 'D', -1, 'bigBroccoli')
+        Jarvis.reset_scheduler()
+        Jarvis.plan(0, add_load_err, (ctl,negativeBroccoli))
+        Jarvis.run()
+
+        # F-10 is badly worded, needs clarification
+        cart = Cart(2, 500, 1)
+        cart.onmove = on_move
+        broccoli1 = createBasicCargo('B', 'D', 50, 'broccoli1')
+        broccoli2 = createBasicCargo('B', 'D', 50, 'broccoli2')
+        broccoli3 = createBasicCargo('B', 'D', 50, 'broccoli3')
+        Jarvis.reset_scheduler()
+        Jarvis.plan(0, add_load, (ctl,broccoli1))
+        Jarvis.plan(1, add_load, (ctl,broccoli2))
+        Jarvis.plan(2, add_load_err, (ctl,broccoli3))
+        Jarvis.run()
+
+        # F-10 is badly worded, needs clarification
+        cart = Cart(2, 500, 1)
+        cart.onmove = on_move
+        broccoli1 = createBasicCargo('B', 'D', 50, 'broccoli1')
+        broccoli2 = createBasicCargo('B', 'D', 50, 'broccoli2')
+        broccoli3 = createBasicCargo('B', 'D', 50, 'broccoli3')
+        Jarvis.reset_scheduler()
+        Jarvis.plan(0, add_load, (ctl,broccoli1))
+        Jarvis.plan(1, add_load, (ctl,broccoli2))
+        Jarvis.plan(35, add_load_err, (ctl,broccoli3))
+        Jarvis.run()
+        
+
+        # C-06 The system shall communicate correctly with the factory’s external systems and respond to their requests.
+        #TODO
+        return
+    
 
 if __name__ == "__main__":
     unittest.main()
